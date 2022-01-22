dfc5fcfc77cd91263961273856baf82395349e6e
Kunal <86222688+Adityanrnsingh@users.noreply.github.com>
2022-01-22 18:25:46 +0530

    tailwind config init

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <button className="bg-blue-400 hover:bg-blue-600 font-bold text-white p-2 rounded">
+        Hello
+      </button>
+    </div>
+  );
+};
+
+export default Home;
