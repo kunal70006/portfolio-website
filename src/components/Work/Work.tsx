5404ff1fe2f3fcd44c0f80d62e0c8684d145fd29
kunal70006 <singkunaldeep@gmail.com>
2022-06-23 01:13:15 +0530

    added work exp, changed resume

--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -6,7 +6,7 @@ const Work = () => {
   return (
     <section
       id="work"
-      className="lg:px-96 sm:px-0 bg-black h-screen flex items-center sm:mt-40 lg:mt-0"
+      className="lg:px-96 sm:px-0 bg-black min-h-screen flex items-center sm:mt-40 lg:mt-0"
       ref={observe}
     >
       {inView && (
@@ -19,37 +19,64 @@ const Work = () => {
               </h1>
               <div className=" lg:ml-4 sm:ml-1 lg:w-80 sm:w-10 border-t border-slate " />
             </div>
-            <div className="flex sm:flex-col lg:flex-row">
-              <div className="flex flex-col lg:pl-20 mt-8 text-lg lg:w-1/2 sm:w-full">
-                <h1 className="text-lightSlate font-bold">
-                  Engineer{" "}
-                  <a
-                    className="text-red cursor-pointer"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://www.cyberoisystems.com/"
-                  >
-                    @ CyberoiSystems (remote)
-                  </a>
-                </h1>
-                <p className="font-mono text-xs mt-2 text-slate">
-                  November 2021 - March 2022
-                </p>
-                <ul className="list-disc text-slate text-base mt-4 pl-4">
-                  <li className="mb-2">
-                    Wrote modern, performant, maintainable code for a diverse
-                    array of clients and internal projects
-                  </li>
-                  <li className="mb-2">
-                    Worked with a variety of different languages, platforms,
-                    frameworks, and version control systems such as JavaScript,
-                    TypeScript, React, NextJS, Gitlab, and Github
-                  </li>
-                  <li className="mb-2">
-                    Communicated with multi-disciplinary teams of engineers,
-                    designers, product managers, and clients on a daily basis
-                  </li>
-                </ul>
+            <div className="mb-52">
+              <div className="flex sm:flex-col lg:flex-row">
+                <div className="flex flex-col lg:pl-20 mt-8 text-lg lg:w-1/2 sm:w-full">
+                  <h1 className="text-lightSlate font-bold">
+                    Front-end Engineer{" "}
+                    <a
+                      className="text-red cursor-pointer"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://www.cyberoisystems.com/"
+                    >
+                      @ CyberoiSystems (remote)
+                    </a>
+                  </h1>
+                  <p className="font-mono text-xs mt-2 text-slate">
+                    November 2021 - March 2022
+                  </p>
+                  <ul className="list-disc text-slate text-base mt-4 pl-4">
+                    <li className="mb-2">
+                      Wrote modern, performant, maintainable code for a diverse
+                      array of clients and internal projects
+                    </li>
+                    <li className="mb-2">
+                      Worked with a variety of different languages, platforms,
+                      frameworks, and version control systems such as
+                      JavaScript, TypeScript, React, NextJS, Gitlab, and Github
+                    </li>
+                    <li className="mb-2">
+                      Communicated with multi-disciplinary teams of engineers,
+                      designers, product managers, and clients on a daily basis
+                    </li>
+                  </ul>
+                </div>
+                <div className="flex flex-col lg:pl-20 mt-8 text-lg lg:w-1/2 sm:w-full">
+                  <h1 className="text-lightSlate font-bold">
+                    Front-end Engineer{" "}
+                    <a
+                      className="text-red "
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      @ The Forestta (remote)
+                    </a>
+                  </h1>
+                  <p className="font-mono text-xs mt-2 text-slate">
+                    November 2020 - November 2021
+                  </p>
+                  <ul className="list-disc text-slate text-base mt-4 pl-4">
+                    <li className="mb-2">
+                      Planned website development, converting mockups into
+                      usable web components for both mobile and desktop
+                      platforms with HTML, CSS, JavaScript and React.
+                    </li>
+                    <li className="mb-2">
+                      Adhered to SEO best practices while designing sites
+                    </li>
+                  </ul>
+                </div>
               </div>
               <div className="flex flex-col lg:pl-20 mt-8 text-lg lg:w-1/2 sm:w-full">
                 <h1 className="text-lightSlate font-bold">
